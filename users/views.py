408602450c2182ade2c2d944a7791ef9eaf7b79a
Diego Barbo <diegobarbox@gmail.com>
2022-06-28 16:15:37 -0300

    adicionando outros campos

--- users/views.py
+++ users/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import redirect, render
 
-from django.contrib.auth.forms import UserCreationForm
-
 from django.contrib import messages
+
+from .forms import RegisterForm
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Welcome {username}. Your account is created.')
             return redirect('dish:index')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     return render (request, 'users/register.html', {'form': form})

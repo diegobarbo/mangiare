8fbadf1d96102a159cbc5fa835d41abc0c9bee0c
Diego Barbo <diegobarbox@gmail.com>
2022-07-05 15:13:25 -0300

    adicionando foto ao perfil do usuario

--- config/urls.py
+++ config/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from re import template
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as authentication_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -28,3 +29,8 @@ urlpatterns = [
     path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.profilepage, name='profile'),
 ]
+
+urlpatterns += [
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
